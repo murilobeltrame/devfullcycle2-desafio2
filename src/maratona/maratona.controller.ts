877009f96586c92b61be8796687867c3f3f47414
Murilo Beltrame <murilobeltrame@gmail.com>
2020-05-07 01:31:54 -0300

    new controller operations

--- src/maratona/maratona.controller.ts
+++ src/maratona/maratona.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Request, Req } from '@nestjs/common';
+import { Controller, Get, Post, Request, Req, Param, Put, Delete } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Maratona } from './maratona.model';
 import { Repository } from 'typeorm';
@@ -21,5 +21,20 @@ export class MaratonaController {
         const maratona = this.maratonaRepository.create(request.body as any)
         await this.maratonaRepository.save(maratona)
         return maratona
+    }
+
+    @Get(':id')
+    async get(@Param('id') id: string) {
+        return await this.maratonaRepository.findOneOrFail({where: {id}})
+    }
+
+    @Put(':id')
+    async put(@Param('id') id: string, @Req() request: Request) {
+        await this.maratonaRepository.update(id, request.body as any)
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        await this.maratonaRepository.delete(id)
     }
 }
